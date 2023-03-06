edf8749f01725021502bc5454a3724aabad8d15e
Bharath <bharath@emproto.com>
2023-03-06 21:34:39 +0530

    bug fix

---
+++ build/precache-manifest.54a32296ad1cc444338e8c9d59d64e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fb1ff8dd32d77daf80bbd6a98eb517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6d2e013e68442c548ff",
+    "url": "/static/css/main.4242bb87.chunk.css"
+  },
+  {
+    "revision": "d5f819902ba80d5f5f33",
+    "url": "/static/js/2.d10ee725.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d10ee725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d2e013e68442c548ff",
+    "url": "/static/js/main.3dc11967.chunk.js"
+  },
+  {
+    "revision": "31b78841b51019e3cb80",
+    "url": "/static/js/runtime-main.153ebfaf.js"
+  }
+]);
