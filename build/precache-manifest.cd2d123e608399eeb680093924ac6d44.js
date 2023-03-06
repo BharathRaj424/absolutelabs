c0efed0026dfa4c97a16a6dff5dc227cc21178df
Bharath <bharath@emproto.com>
2023-03-06 21:36:10 +0530

    asd

---
+++ build/precache-manifest.cd2d123e608399eeb680093924ac6d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b4664442833a416f2b93e69d89955a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a04b97faf775a18ad83",
+    "url": "/static/css/main.4242bb87.chunk.css"
+  },
+  {
+    "revision": "d5f819902ba80d5f5f33",
+    "url": "/static/js/2.d10ee725.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d10ee725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a04b97faf775a18ad83",
+    "url": "/static/js/main.8550d387.chunk.js"
+  },
+  {
+    "revision": "31b78841b51019e3cb80",
+    "url": "/static/js/runtime-main.153ebfaf.js"
+  }
+]);
