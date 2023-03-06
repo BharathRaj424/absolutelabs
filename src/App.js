c9768f756bb12b697fa9ef08ad191b1f7684bc22
Bharath <bharath@emproto.com>
2023-03-06 21:02:17 +0530

    implemented ci cd pipeline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './App.css';
-import AppRouter from './components/AppRouter.js';
+import React from "react";
+import "./App.css";
+import AppRouter from "./components/AppRouter.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     return (
-      <div className="App">          
+      <div className="App">
         <Router>
           <AppRouter></AppRouter>
         </Router>
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './../css/Home.css';
+import React from "react";
+import "./../css/Home.css";
 
 class Home extends React.Component {
-   render() {
-      return (
-         <div className='home-container'>
-            Hello Static Website!
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div className="home-container">
+        Hello Static Website! and Implemeneted CI/CD Pipeline
+      </div>
+    );
+  }
 }
 
 export default Home;
